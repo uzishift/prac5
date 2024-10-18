c8794282a7353591d8595ed7bd79f0d7ca9f4539
ИСП-31 <-31@LAB17-12>
2024-10-18 14:45:26 -0600

    Обновлена логика программы

--- prac5/MainWindow.xaml.cs
+++ prac5/MainWindow.xaml.cs
@@ -1,40 +1,45 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System;
-using System.Windows;
+﻿using System.Windows;
 
 namespace prac5
 {
-    /// <summary>
-    /// Основной класс окна приложения.
-    /// </summary>
     public partial class MainWindow : Window
-    {
+    {   
         /// <summary>
-        /// Первая триада.
+        /// Объявление Триады 1.
         /// </summary>
         private Triad _triad1;
         /// <summary>
-        /// Вторая триада
+        /// Объявление Триады 2.
         /// </summary>
         private Triad _triad2;
+
         /// <summary>
-        /// Конструктор класса MainWindow.
+        /// Конструктор MainWindow.
         /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+            _triad1 = new Triad(0, 0, 0);
+            _triad2 = new Triad(0, 0, 0);
+            InitializeTextBoxes();
         }
+
         /// <summary>
-        /// Кнопка выхода из приложения.
+        /// Метод инициализации TextBox'ов
+        /// </summary>
+        private void InitializeTextBoxes()
+        {
+            Triad1First.Text = _triad1.First.ToString();
+            Triad1Second.Text = _triad1.Second.ToString();
+            Triad1Third.Text = _triad1.Third.ToString();
+
+            Triad2First.Text = _triad2.First.ToString();
+            Triad2Second.Text = _triad2.Second.ToString();
+            Triad2Third.Text = _triad2.Third.ToString();
+        }
+
+        /// <summary>
+        /// Кнопка выхода из программы.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -42,42 +47,32 @@ namespace prac5
         {
             this.Close();
         }
+
         /// <summary>
-        /// Кнопка вывода информации о программе.
+        /// Кнопка информации о программе
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void btnInfo_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Разработчик: Демьяхин Роман ИСП-31 12 Вариант\nСоздать класс Triad (тройка чисел). Создать необходимые методы и свойства.\r\nОпределить методы с операциями сложения с числом, умножения на число, проверки на равенство.\nСоздать перегруженный метод для сложения элементов одой триады с другой триадой.", "О программе");
+            MessageBox.Show("Разработчик: Демьяхин Роман ИСП-31 12 Вариант\nСоздать класс Triad (тройка чисел). Создать необходимые методы и свойства.\r\nОпределить методы с операциями сложения с числом, умножения на число, проверки на равенство.\nСоздать перегруженный метод для сложения элементов одной триады с другой триадой.", "О программе");
         }
+
         /// <summary>
-        /// Кнопка сложения триад.
+        /// Кнопка сложения Триад.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void btnAddTriads_Click(object sender, RoutedEventArgs e)
         {
-            try
+            if (UpdateTriads())
             {
-                _triad1 = new Triad(
-                    int.Parse(Triad1First.Text),
-                    int.Parse(Triad1Second.Text),
-                    int.Parse(Triad1Third.Text));
-
-                _triad2 = new Triad(
-                    int.Parse(Triad2First.Text),
-                    int.Parse(Triad2Second.Text),
-                    int.Parse(Triad2Third.Text));
-
-                Triad result = _triad1.AddTriad(_triad2);
-                tbOutput.Text = $"Сложение Триад:\n{_triad1} + {_triad2} = {result}";
+                _triad1.AddTriad(_triad2);
+                tbOutput.Text = $"{_triad1}";
             }
-            catch (Exception)
-            {
-                MessageBox.Show("Введите корректные числа для обеих триад.", "Ошибка ввода");
-            }
+            UpdateTextBoxesForTriad1();
         }
+
         /// <summary>
         /// Кнопка умножения Триады 1 на число.
         /// </summary>
@@ -85,28 +80,18 @@ namespace prac5
         /// <param name="e"></param>
         private void btnMultiplyTriad1_Click(object sender, RoutedEventArgs e)
         {
-            try
+            if (UpdateTriad1() && int.TryParse(tbNumber.Text, out int number))
             {
-                _triad1 = new Triad(
-                    int.Parse(Triad1First.Text),
-                    int.Parse(Triad1Second.Text),
-                    int.Parse(Triad1Third.Text));
-
-                if (int.TryParse(tbNumber.Text, out int number))
-                {
-                    Triad result = _triad1.MultiplyByNumber(number);
-                    tbOutput.Text = $"Умножение Триады 1 на {number}:\n{_triad1} * {number} = {result}";
-                }
-                else
-                {
-                    MessageBox.Show("Введите корректное число для операции умножения.", "Ошибка ввода");
-                }
+                _triad1.MultiplyByNumber(number);
+                tbOutput.Text = $"Умножение Триады 1 на {number}:\n{_triad1}";
+                UpdateTextBoxesForTriad1();
             }
-            catch (Exception)
+            else
             {
-                MessageBox.Show("Введите корректные числа для триады.", "Ошибка ввода");
+                MessageBox.Show("Введите корректное число для операции умножения.", "Ошибка ввода");
             }
         }
+
         /// <summary>
         /// Кнопка сравнения триад.
         /// </summary>
@@ -114,54 +99,98 @@ namespace prac5
         /// <param name="e"></param>
         private void btnCompareTriads_Click(object sender, RoutedEventArgs e)
         {
-            try
+            if (UpdateTriads())
             {
-                _triad1 = new Triad(
-                    int.Parse(Triad1First.Text),
-                    int.Parse(Triad1Second.Text),
-                    int.Parse(Triad1Third.Text));
-
-                _triad2 = new Triad(
-                    int.Parse(Triad2First.Text),
-                    int.Parse(Triad2Second.Text),
-                    int.Parse(Triad2Third.Text));
-
                 bool isEqual = _triad1.Equals(_triad2);
                 tbOutput.Text = $"Сравнение Триад:\n{_triad1} и {_triad2} равны? {isEqual}";
             }
-            catch (Exception)
-            {
-                MessageBox.Show("Введите корректные числа для обеих триад.", "Ошибка ввода");
-            }
         }
+
         /// <summary>
-        /// Кнопка сложения триады 1 с числом.
+        /// Кнопка сложения триады 1 с числом
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void btnAddNumberToTriad1_Click(object sender, RoutedEventArgs e)
         {
-            try
+            if (UpdateTriad1() && int.TryParse(tbNumber.Text, out int number))
             {
-                _triad1 = new Triad(
-                    int.Parse(Triad1First.Text),
-                    int.Parse(Triad1Second.Text),
-                    int.Parse(Triad1Third.Text));
-
-                if (int.TryParse(tbNumber.Text, out int number))
-                {
-                    Triad result = _triad1.AddNumber(number);
-                    tbOutput.Text = $"Сложение Триады 1 с числом {number}:\n{_triad1} + {number} = {result}";
-                }
-                else
-                {
-                    MessageBox.Show("Введите корректное число для операции сложения.");
-                }
+                _triad1.AddNumber(number);
+                tbOutput.Text = $"Сложение Триады 1 с числом {number}:\n{_triad1}";
+                UpdateTextBoxesForTriad1();
             }
-            catch (Exception)
+            else
+            {
+                MessageBox.Show("Введите корректное число для операции сложения.", "Ошибка ввода");
+            }
+        }
+
+        /// <summary>
+        /// Метод обновления триад.
+        /// </summary>
+        /// <returns></returns>
+        private bool UpdateTriads()
+        {
+            bool isValid = true;
+
+            isValid &= int.TryParse(Triad1First.Text, out int triad1First);
+            isValid &= int.TryParse(Triad1Second.Text, out int triad1Second);
+            isValid &= int.TryParse(Triad1Third.Text, out int triad1Third);
+
+            isValid &= int.TryParse(Triad2First.Text, out int triad2First);
+            isValid &= int.TryParse(Triad2Second.Text, out int triad2Second);
+            isValid &= int.TryParse(Triad2Third.Text, out int triad2Third);
+
+            if (!isValid)
+            {
+                MessageBox.Show("Введите корректные числа для обеих триад.", "Ошибка ввода");
+                return false;
+            }
+
+            _triad1.First = triad1First;
+            _triad1.Second = triad1Second;
+            _triad1.Third = triad1Third;
+
+            _triad2.First = triad2First;
+            _triad2.Second = triad2Second;
+            _triad2.Third = triad2Third;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Метод обновления 1 триады.
+        /// </summary>
+        /// <returns></returns>
+        private bool UpdateTriad1()
+        {
+            bool isValid = true;
+
+            isValid &= int.TryParse(Triad1First.Text, out int triad1First);
+            isValid &= int.TryParse(Triad1Second.Text, out int triad1Second);
+            isValid &= int.TryParse(Triad1Third.Text, out int triad1Third);
+
+            if (!isValid)
             {
                 MessageBox.Show("Введите корректные числа для триады.", "Ошибка ввода");
+                return false;
             }
+
+            _triad1.First = triad1First;
+            _triad1.Second = triad1Second;
+            _triad1.Third = triad1Third;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Метод обновления TextBox'ов 1 триады.
+        /// </summary>
+        private void UpdateTextBoxesForTriad1()
+        {
+            Triad1First.Text = _triad1.First.ToString();
+            Triad1Second.Text = _triad1.Second.ToString();
+            Triad1Third.Text = _triad1.Third.ToString();
         }
     }
 }
--- prac5/Triad.cs
+++ prac5/Triad.cs
@@ -15,10 +15,12 @@ namespace prac5
         /// Первое число триады.
         /// </summary>
         public int First { get; set; }
+
         /// <summary>
         /// Второе число триады.
         /// </summary>
         public int Second { get; set; }
+
         /// <summary>
         /// Третье число триады.
         /// </summary>
@@ -27,9 +29,9 @@ namespace prac5
         /// <summary>
         /// Конструктор для инициализации триады.
         /// </summary>
-        /// <param name="first">Первое число триады.</param>
-        /// <param name="second">Второе число триады.</param>
-        /// <param name="third">Третье число триады.</param>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <param name="third"></param>
         public Triad(int first, int second, int third)
         {
             First = first;
@@ -41,26 +43,42 @@ namespace prac5
         /// Метод для сложения всех элементов триады с числом.
         /// </summary>
         /// <param name="number">Число, которое будет добавлено ко всем элементам триады.</param>
-        /// <returns>Новая триада с добавленным числом к каждому элементу.</returns>
-        public Triad AddNumber(int number)
+        public void AddNumber(int number)
         {
-            return new Triad(First + number, Second + number, Third + number);
+            First += number;
+            Second += number;
+            Third += number;
         }
 
         /// <summary>
         /// Метод для умножения всех элементов триады на число.
         /// </summary>
         /// <param name="number">Число, на которое будут умножены все элементы триады.</param>
-        /// <returns>Новая триада с умноженными элементами.</returns>
-        public Triad MultiplyByNumber(int number)
+        public void MultiplyByNumber(int number)
         {
-            return new Triad(First * number, Second * number, Third * number);
+            First *= number;
+            Second *= number;
+            Third *= number;
+        }
+
+        /// <summary>
+        /// Метод для сложения двух триад.
+        /// </summary>
+        /// <param name="other">Триада, которую нужно сложить с текущей.</param>
+        public void AddTriad(Triad other)
+        {
+            if (other != null)
+            {
+                First += other.First;
+                Second += other.Second;
+                Third += other.Third;
+            }
         }
 
         /// <summary>
         /// Метод для проверки равенства двух триад.
         /// </summary>
-        /// <param name="other">Триада для сравнения.</param>
+        /// <param name="other"></param>
         /// <returns>True, если триады равны; иначе False.</returns>
         public bool Equals(Triad other)
         {
@@ -68,19 +86,6 @@ namespace prac5
                 return false;
 
             return First == other.First && Second == other.Second && Third == other.Third;
-        }
-
-        /// <summary>
-        /// Перегруженный метод для сложения двух триад.
-        /// </summary>
-        /// <param name="other">Триада, которую нужно сложить с текущей.</param>
-        /// <returns>Новая триада, являющаяся суммой двух триад, или null, если переданная триада равна null.</returns>
-        public Triad AddTriad(Triad other)
-        {
-            if (other is null)
-                return null;
-
-            return new Triad(First + other.First, Second + other.Second, Third + other.Third);
         }
 
         /// <summary>
